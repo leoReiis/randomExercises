209bbfb5a97c30b911a23cf54bae29aa55c137d9
Leonardo Reis <leonardoreis.ufsj@gmail.com>
2023-06-21 20:55:08 -0300

    primeiro commit

---
+++ cloneInstagram/index.js
@@ -0,0 +1,8 @@
+function clicarNoBotao() {
+  let email = document.getElementById("campo-email").value;
+  let senha = document.getElementById("campo-senha").value;
+
+  if (email === "") {
+    alert("Email inválido");
+  }
+}
